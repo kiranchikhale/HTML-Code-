8464c17639225a6b91203d91ab98d20f40d01237
Kiran Ch <kiran.chikhale@gmail.com>
2023-08-06 17:44:05 +0530

    Add files via upload

---
+++ html.php
@@ -0,0 +1,5 @@
+What is HTML?
+
+Hypertext Markup Language is the standard markup language for creating web pages.
+
+Hypertext means machine readable text and Markup means to structure it in a specific format.So,HTML is called hypertext markup language because it is a language that allows users to organize, improve the appearance of, and link text with data on the internet.
